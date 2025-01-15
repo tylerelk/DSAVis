52836f898dd6f8673a65da050e65de9c6bf75b63
Tyler Lewis <tylerlewisrn@gmail.com>
2025-01-15 15:34:25 -0500

    Complete Bubble Sort

--- backend/dsalogic/BubbleSort.py
+++ backend/dsalogic/BubbleSort.py
@@ -8,16 +8,15 @@ Analysis
     - Space complexity: O(n)
 '''
 def bubbleSort(arr):
-    if len(arr) < 2:
+    n = len(arr)
+    if n < 2:
         return arr
-    swapped = True
-    while swapped:
-        for i in range(0, len(arr) - 1):
-            if arr[i] > arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
-                pass
-            swapped = False
+        if not swapped:
+            break
     return arr
-
-print(bubbleSort([5, 2, 6, 1, 3]))
